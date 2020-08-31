49dc7001eb815f08915a5de8e07a6becfd9c88b0
Lighty <kh.ultra@gmail.com>
2020-08-31 14:56:19 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
      mode: "development",
-     entry:'./assets/js/shader.js',
+     entry:'./assets/js/plugins.js',
      output: {
           filename: "bundle.js",
           path: path.resolve(__dirname, "dist")
@@ -13,23 +13,14 @@ module.exports = {
      module: {
           rules: [
                {
-                    test: /\.(frag|vert|glsl)$/,
-                    use: [
-                         {
-                              loader: 'glsl-shader-loader',
-                              options: {}
-                         }
-                    ]
+                    test: /\.glsl$/,
+                    loader: 'webpack-glsl-loader'
+               },
+               {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: '/node_modules'
                }
-               // {
-               //      test: /\.(glsl|vs|fs|vert|frag)$/,
-               //      exclude: /node_modules/,
-               //      use: [
-               //           'raw-loader',
-               //           'glslify-loader'
-               //      ]
-               // },
-
           ]
      }
 }
