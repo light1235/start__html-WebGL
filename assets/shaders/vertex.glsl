43c5353dd07db7e99fca909d9696072c37270254
Lighty <kh.ultra@gmail.com>
2020-08-31 15:06:17 +0300

    Add files via upload

--- assets/shaders/vertex.glsl
+++ assets/shaders/vertex.glsl
@@ -1,7 +1,7 @@
-precision highp float;
-attribute vec3 position;
-varying vec2 vUv;
-void main () {
-gl_Position = vec4(position.xyz, 1.0);
-  vUv = gl_Position.xy * 0.5 + 0.5;
-}
+ varying vec2 vUv;
+ uniform float time;
+ void main() {
+ vUv = uv;
+ vec3 pos = position.xyz * sin(time);
+ gl_Position = projectionMatrix * modelViewMatrix * vec4(position.xyz, 1.0);
+ }
