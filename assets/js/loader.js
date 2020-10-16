a33d5d1a522cf0bce7efbd91dd857fd501deb703
Lighty <kh.ultra@gmail.com>
2020-10-16 17:26:00 +0300

    Update loader.js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,26 +1,30 @@
+
+//Model-loader/////////////////////////////////////////////////////////
 hlight = new THREE.AmbientLight (0x404040,100);
 scene.add(hlight);
-
-
-var loader = new THREE.GLTFLoader();
+{
+     const loader = new THREE.GLTFLoader();
 
 //standart
-loader.load(
-     'scene.gltf',
-     function ( object ) {
-          // called when the resource is loaded
-          scene.add( object.scene );
-     },
+     loader.load(
+          'scene.gltf',
+          function ( object ) {
+               // called when the resource is loaded
+               scene.add( object.scene );
+          },
 
-);
+     );
+
+}
 
 
 //---------------------------
 
-var loader = new THREE.GLTFLoader();
+//Model-loader-V2//////////////////////////////////////////////////////////
+let loader = new THREE.GLTFLoader();
 loader.load('scene.gltf', handle_load);
 
-var mesh;
+let mesh;
 
 function handle_load(gltf){
      mesh = gltf.scene.children[0];
@@ -31,9 +35,7 @@ function handle_load(gltf){
 }
 
 //---------------------------
-//TextureLoader
-
-//V1
+//TextureLoader////////////////////////////////////////////////////////
 let sprite = new THREE.TextureLoader().load('../assets/img/starts.png');
 
 const material = new  THREE.PointsMaterial({
@@ -41,32 +43,56 @@ const material = new  THREE.PointsMaterial({
      size:0.7,
      map:sprite
 })
+//TextureLoaderV2/////////////////////////////////////////////////////////////
 
-//V2
+{
+     const material = new  THREE.PointsMaterial({
+          color:0xaaaaaa,
+          size:0.7,
+          map:new  THREE.TextureLoader().load('../assets/img/1.jpg')
+     })
+}
 
+//font-loader////////////////////////////////////////////////////////////
 
-const material = new  THREE.PointsMaterial({
-     color:0xaaaaaa,
-     size:0.7,
-     map:new  THREE.TextureLoader().load('../assets/img/1.jpg')
-})
+{
+     let loader = new THREE.FontLoader();
 
+     loader.load( '../assets/js/font.json', function ( font ) {
 
-//font-loader
-//https://gero3.github.io/facetype.js/
-
-const fontLoader = new THREE.FontLoader();
-
-fontLoader.load( '../assets/js/font.json', function ( font ) {
-
-     var geometry = new THREE.TextGeometry( 'Hello three.js!', {
-          font: font,
-          size: 4.05,
-          height: 0.01,
+          var geometry = new THREE.TextGeometry( 'Hello three.js!', {
+               font: font,
+               size: 4.05,
+               height: 0.01,
+          } );
+          const material = new   THREE.MeshBasicMaterial( {color: 0xff0000});
+          const mesh = new THREE.Mesh(geometry,material);
+          // mesh.position.x = 2 / window.innerWidth;
+          geometry.center();
+          // mesh.position.y = window.innerHeight / 4;
+          scene.add(mesh);
+          scene.add(new THREE.AxesHelper(500,500));
      } );
-     const material = new   THREE.MeshBasicMaterial( {color: 0xff0000});
-     const mesh = new THREE.Mesh(geometry,material);
-     geometry.center();
-     scene.add(mesh);
-     scene.add(new THREE.AxesHelper(500,500));
-});
+}
+
+
+//font-loader with mesh Accessing
+
+{
+     let text;
+
+     fontLoader.load(
+          '../assets/js/font.json',
+          function(font) {
+               var geometry = new THREE.TextGeometry('BLASTER!', {
+                    font: font,
+                    size: 8.2,
+                    height: 3,
+               });
+               var material = new THREE.MeshBasicMaterial({color: 0xff0000});
+               text = new THREE.Mesh(geometry, material);
+               geometry.center();
+               scene.add(text);
+          },
+     );
+}
