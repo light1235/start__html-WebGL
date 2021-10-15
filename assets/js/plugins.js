5ab814912211770f1d67100375f28e79ac9d06cf
Lighty <kh.ultra1234@gmail.com>
2021-10-15 15:06:31 +0300

    Update plugins.js

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,12 +1,10 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-
-
+// camera.position.setScalar(25);
 
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor(0xECF0F1);
+renderer.setClearColor(0x202020);
 let doc = document.querySelector('.main');
 doc.appendChild( renderer.domElement );
 
@@ -20,6 +18,12 @@ window.addEventListener('resize', function () {
      camera.updateProjectionMatrix();
 });
 
+
+let light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.setScalar(1);
+scene.add(light, new THREE.AmbientLight(0xffffff, 0.5));
+
+scene.add(new THREE.GridHelper(20, 20));
 
 let ball = {
      rotationY:0,
@@ -51,7 +55,6 @@ gui.add(ball,'rotationY').min(-0.2).max(0.2).step(0.001);
 let controls = new  THREE.OrbitControls(camera,renderer.domElement);
 
 
-
 const geometry = new THREE.BoxGeometry( 5,5,5 );
 const material = new  THREE.MeshNormalMaterial({wireframe: true});
 // const material = new  THREE.ShaderMaterial({
@@ -67,12 +70,16 @@ scene.add(mesh);
 
 
 
+
 camera.position.z = 20;
+const clock = new THREE.Clock();
 
 const animate = function () {
      requestAnimationFrame( animate );
-     // mesh.rotation.x += 0.01;
+     // controls.update();
+     mesh.rotation.y = clock.getElapsedTime();
      // mesh.rotation.y += 0.01;
+
      mesh.rotation.y += ball.rotationY;
      renderer.render( scene, camera );
 };
