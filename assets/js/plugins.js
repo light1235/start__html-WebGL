376c779ab499ca42b341ac24f6548c9981ff4500
Lighty <kh.ultra1234@gmail.com>
2023-05-01 16:12:16 +0300

    Update plugins.js

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,4 +1,5 @@
-import * as THREE from 'three/build/three.module.js';
+// import * as THREE from 'three/build/three.module.js';
+import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
