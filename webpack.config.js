740bca7dcd6eac280679b65dcf172ee6027be273
Lighty <kh.shlyakhov.v@gmail.com>
2025-01-03 11:10:57 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
      },
      devServer: {
           port:4200
+            devMiddleware: {
+               writeToDisk: true, 
+          },
      },
      module: {
           rules: [
